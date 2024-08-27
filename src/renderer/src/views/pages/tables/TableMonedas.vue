8af2c8f6f411e5a2d6abb346dd290ecbaff0efa1
Junior Pezantes <ti15@emilima.com.pe>
2024-08-26 19:02:48 -0500

    Fix: TableMonedas

--- src/renderer/src/views/pages/tables/TableMonedas.vue
+++ src/renderer/src/views/pages/tables/TableMonedas.vue
@@ -1,26 +1,31 @@
 <script setup lang="ts">
 import { ref, watch, computed } from 'vue';
+import coin010 from '@/assets/images/monedas/0.10.png';
+import coin020 from '@/assets/images/monedas/0.20.png';
+import coin050 from '@/assets/images/monedas/0.50.png';
+import coin100 from '@/assets/images/monedas/1.00.png';
+import coin200 from '@/assets/images/monedas/2.00.png';
+import coin500 from '@/assets/images/monedas/5.00.png';
 
 const coins = [
-  { value: 0.10, quantity: ref<number | null>(null), amount: ref<number>(0) },
-  { value: 0.20, quantity: ref<number | null>(null), amount: ref<number>(0) },
-  { value: 0.50, quantity: ref<number | null>(null), amount: ref<number>(0) },
-  { value: 1.00, quantity: ref<number | null>(null), amount: ref<number>(0) },
-  { value: 2.00, quantity: ref<number | null>(null), amount: ref<number>(0) },
-  { value: 5.00, quantity: ref<number | null>(null), amount: ref<number>(0) }
+  { value: 0.10, quantity: ref<number | null>(null), amount: ref<number>(0), image: coin010 },
+  { value: 0.20, quantity: ref<number | null>(null), amount: ref<number>(0), image: coin020 },
+  { value: 0.50, quantity: ref<number | null>(null), amount: ref<number>(0), image: coin050 },
+  { value: 1.00, quantity: ref<number | null>(null), amount: ref<number>(0), image: coin100 },
+  { value: 2.00, quantity: ref<number | null>(null), amount: ref<number>(0), image: coin200 },
+  { value: 5.00, quantity: ref<number | null>(null), amount: ref<number>(0), image: coin500 }
 ];
 
-coins.forEach(bill => {
-  watch(bill.quantity, (newQuantity) => {
-    bill.amount.value = newQuantity !== null ? newQuantity * bill.value : 0;
+coins.forEach(coin => {
+  watch(coin.quantity, (newQuantity) => {
+    coin.amount.value = newQuantity !== null ? newQuantity * coin.value : 0;
   });
 });
 
-// Calculamos el total de todos los billetes
+// Calculamos el total de todas las monedas
 const totalAmount = computed(() =>
-  coins.reduce((sum, bill) => sum + bill.amount.value, 0)
+  coins.reduce((sum, coin) => sum + coin.amount.value, 0)
 );
-
 </script>
 
 <template>
@@ -28,25 +33,25 @@ const totalAmount = computed(() =>
     <thead>
       <tr>
         <th class="text-uppercase"></th>
-        <th class="text-uppercase text-center">cantidad</th>
-        <th class="text-uppercase text-center">monto (S/.)</th>
+        <th class="text-uppercase text-center">Cantidad</th>
+        <th class="text-uppercase text-center">Monto (S/.)</th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(bill, index) in coins" :key="index">
+      <tr v-for="(coin, index) in coins" :key="index">
         <td>
-          <img :src="`../../../assets/images/billetes/${bill.value}.png`" :alt="`imagen referencial de moneda de ${bill.value}`">
+          <img :src="coin.image" :alt="`Imagen referencial de moneda de ${coin.value}`" />
         </td>
         <td class="text-center">
           <VTextField
-            v-model="bill.quantity"
+            v-model="coin.quantity"
             type="number"
             class="text-end"
           />
         </td>
         <td class="text-center center-amount">
           <VTextField
-            :model-value="bill.amount"
+            :model-value="coin.amount"
             type="number"
             class="text-end"
             readonly
@@ -56,7 +61,7 @@ const totalAmount = computed(() =>
     </tbody>
     <tfoot>
       <tr>
-        <td colspan="2" class="text-uppercase">total de monedas:</td>
+        <td colspan="2" class="text-uppercase">Total de monedas:</td>
         <td class="text-center">
           <VTextField
             :model-value="totalAmount"
